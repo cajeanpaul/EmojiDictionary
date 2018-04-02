1e663eecb8148e34f52abd09099236800845cf99
Carmelle Jean-Paul <cajeanpaul@gmail.com>
2018-04-02 00:31:09 -0400

    Completed Emoji Dictionary (I Think)! How this would be most complete to me is if I had the ability to sort based on the properties I put in!

--- Emoji Dictionary/EmojiDefinitionViewController.swift
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -14,104 +14,17 @@ class EmojiDefinitionViewController: UIViewController {
     @IBOutlet weak var birthLabel: UILabel!
     @IBOutlet weak var emojiDefinitionLabel: UILabel!
     @IBOutlet weak var emojiLabel: UILabel!
-    var emoji = ""
+    var emoji = Emoji() 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       emojiLabel.text = emoji
-      // ["😂", "😊", "😘", "🤬", "😫", "😈", "✌🏾", "🙏🏾", "👶🏾", "👰🏾", "🤷🏾‍♀️", "🙅🏾‍♀️", "💃🏾", "🤦🏾‍♀️", "☔️", "💕", "🇭🇹", "💸", "💳", "🙌🏾", "👀", "👊🏾"]
         
-        if emoji == "😊" {
-            emojiDefinitionLabel.text = "When you are happy but showing humility"
-            birthLabel.text = "Birth Year: 2005"
-            categoryLabel.text = "Category: Faces"
-        }
-      
-        if emoji == "😘" {
-            emojiDefinitionLabel.text = "I love you and sending you kisses"
-            }
+        emojiLabel.text = emoji.emojis
+        categoryLabel.text = "Category: \(emoji.category)"
+        emojiDefinitionLabel.text = emoji.emojiDefinition
+        birthLabel.text = "Birth Year: \(emoji.birthYear)"
         
-        if emoji == "🤬" {
-            emojiDefinitionLabel.text = "I'm so mad I want to curse!"
-        }
-        
-        if emoji == "😫" {
-            emojiDefinitionLabel.text = "No! Why? Mixture of whinning and crying"
-        }
-        
-        if emoji == "😈" {
-            emojiDefinitionLabel.text = "I'm up to no good. Feeling playfully develish"
-        }
-        if emoji == "✌🏾" {
-            emojiDefinitionLabel.text = "Deuces. Peace Out. I'm Done Arguing"
-            birthLabel.text = "Birth Year: 2014"
-            categoryLabel.text = "Category: Hands"
-        }
-        
-        if emoji == "🙏🏾" {
-            emojiDefinitionLabel.text = "Please God! Prayers Up."
-        }
-        
-        if emoji == "👶🏾" {
-            emojiDefinitionLabel.text = "Baby. Rajesh my future😊"
-            birthLabel.text = "Birth Year: 2018"
-            categoryLabel.text = "Category: People"
-        }
-        
-        if emoji == "👰🏾" {
-            emojiDefinitionLabel.text = "The Bride. Also my future😊"
-        }
-        
-        if emoji == "🤷🏾‍♀️" {
-            emojiDefinitionLabel.text = "Sarcastic I don't know"
-        }
-        
-        if emoji == "🙅🏾‍♀️" {
-            emojiDefinitionLabel.text = "Wakanda Forver! You've got the wrong one!"
-        }
-        
-        if emoji == "💃🏾" {
-            emojiDefinitionLabel.text = "Hey Now! Dancing. Party Time!"
-        }
-        
-        if emoji == "🤦🏾‍♀️" {
-            emojiDefinitionLabel.text = "Duh! You're kidding me?"
-        }
-        
-        if emoji == "☔️" {
-            emojiDefinitionLabel.text = "Rainy Day. Emergency Savings."
-        }
-        
-        if emoji == "💕" {
-            emojiDefinitionLabel.text = "I Love You! Love it!"
-        }
-        
-        if emoji == "🇭🇹" {
-            emojiDefinitionLabel.text = "Haiti! My Family's Country."
-        }
-        
-        if emoji == "💸" {
-            emojiDefinitionLabel.text = "Money! Make it Rain!"
-        }
-        
-        if emoji == "💳" {
-            emojiDefinitionLabel.text = "Credit Card"
-        }
-        
-        if emoji == "🙌🏾" {
-            emojiDefinitionLabel.text = "Hey Now! Frickin Awesome!"
-        }
-        
-        if emoji == "👀" {
-            emojiDefinitionLabel.text = "Nosey. Following. Oooh."
-        }
-        
-        if emoji == "👊🏾" {
-            emojiDefinitionLabel.text = "Boom. I got you."
-        }
-        
-        
+       
     }
 
     
--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
-    var emojis = ["😂", "😊", "😘", "🤬", "😫", "😈", "✌🏾", "🙏🏾", "👶🏾", "👰🏾", "🤷🏾‍♀️", "🙅🏾‍♀️", "💃🏾", "🤦🏾‍♀️", "☔️", "💕", "🇭🇹", "💸", "💳", "🙌🏾", "👀", "👊🏾"]
+    var emojis : [Emoji] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        emojis = createEmojis()
         
     }
 
@@ -27,8 +27,11 @@ class EmojiTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        let emoji = emojis[indexPath.row]
 
-      cell.textLabel?.text = emojis[indexPath.row]
+        
+      cell.textLabel?.text = "\(emoji.emojis)"
         
         return cell
     }
@@ -40,8 +43,143 @@ class EmojiTableViewController: UITableViewController {
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let emojiDefVC = segue.destination as! EmojiDefinitionViewController
-        emojiDefVC.emoji = sender as! String
+        emojiDefVC.emoji = sender as! Emoji
     }
 
-     
+    func createEmojis() -> [Emoji] {
+        let crackingUp = Emoji()
+        crackingUp.emojis = "😂"
+        crackingUp.emojiDefinition = "Cracking Up! Laughing my ass off!"
+        crackingUp.birthYear = 2002
+        crackingUp.category = "Faces"
+        
+        let blush = Emoji()
+        blush.emojis = "😊"
+        blush.emojiDefinition = "When you are happy but showing humility"
+        blush.birthYear = 2005
+        blush.category = "Faces"
+        
+        let peace = Emoji()
+        peace.emojis = "✌🏾"
+        peace.emojiDefinition = "Deuces. Peace Out. I'm Done Arguing."
+        peace.birthYear = 2014
+        peace.category = "Hands"
+        
+        let kisses = Emoji()
+        kisses.emojis = "😘"
+        kisses.emojiDefinition = "I love you and sending you kisses!"
+        kisses.birthYear = 2017
+        kisses.category = "Faces"
+        
+        let cussing = Emoji()
+        cussing.emojis = "🤬"
+        cussing.emojiDefinition = "I'm so mad I want to curse!"
+        cussing.birthYear = 2008
+        cussing.category = "Faces"
+        
+        let why = Emoji()
+        why.emojis = "😫"
+        why.emojiDefinition = "No! Why? Mixture of whinning and crying."
+        why.birthYear = 2015
+        why.category = "Faces"
+        
+        let impish = Emoji()
+        impish.emojis = "😈"
+        impish.emojiDefinition = "I'm up to no good! Feeling playfully develish."
+        impish.birthYear = 2003
+        impish.category = "Faces"
+        
+        let prayer = Emoji()
+        prayer.emojis = "🙏🏾"
+        prayer.emojiDefinition = "Please God! Prayers up."
+        prayer.birthYear = 2011
+        prayer.category = "Hands"
+        
+        let baby = Emoji()
+        baby.emojis = "👶🏾"
+        baby.emojiDefinition = "Baby. Rajesh my future😊"
+        baby.birthYear = 2018
+        baby.category = "People"
+        
+        let bride = Emoji()
+        bride.emojis = "👰🏾"
+        bride.emojiDefinition = "The Bride. Also my future😊"
+        bride.birthYear = 2018
+        bride.category = "People"
+        
+        let iDontKnow = Emoji()
+        iDontKnow.emojis = "🤷🏾‍♀️"
+        iDontKnow.emojiDefinition = "Sarcastic I don't know"
+        iDontKnow.birthYear = 2012
+        iDontKnow.category = "Gesture"
+        
+        let never = Emoji()
+        never.emojis = "🙅🏾‍♀️"
+        never.emojiDefinition = "Wakanda Forever! Never! You've got the wrong one!"
+        never.birthYear = 2018
+        never.category = "Gesture"
+        
+        let dancing = Emoji()
+        dancing.emojis = "💃🏾"
+        dancing.emojiDefinition = "Hey Now! Dancing. Party Time!"
+        dancing.birthYear = 2005
+        dancing.category = "Action"
+        
+        let haiti = Emoji()
+        haiti.emojis = "🇭🇹"
+        haiti.emojiDefinition = "Haiti. My Family's Country. 1st Black Free Nation!"
+        haiti.birthYear = 1804
+        haiti.category = "Flag"
+        
+        let money = Emoji()
+        money.emojis = "💸"
+        money.emojiDefinition = "Money! Make it Rain!"
+        money.birthYear = 2017
+        money.category = "Finance"
+        
+        let credit = Emoji()
+        credit.emojis = "💳"
+        credit.emojiDefinition = "Credit Card. Keep it clean!"
+        credit.birthYear = 2002
+        credit.category = "Finance"
+        
+        
+        let awesome = Emoji()
+        awesome.emojis = "🙌🏾"
+        awesome.emojiDefinition = "Hey Now! Frickin Awesome!"
+        awesome.birthYear = 2009
+        awesome.category = "Hands"
+        
+        let eyes = Emoji()
+        eyes.emojis = "👀"
+        eyes.emojiDefinition = "Nosey. Following. Oooh."
+        eyes.birthYear = 2013
+        eyes.category = "Action"
+        
+        let fist = Emoji()
+        fist.emojis = "👊🏾"
+        fist.emojiDefinition = "Boom! I got you!"
+        fist.birthYear = 2011
+        fist.category = "Hands"
+        
+        let duh = Emoji()
+        duh.emojis = "🤦🏾‍♀️"
+        duh.emojiDefinition = "Duh! You're kidding me!"
+        duh.birthYear = 2016
+        duh.category = "Gesture"
+        
+        let rainyDay = Emoji()
+        rainyDay.emojis = "☔️"
+        rainyDay.emojiDefinition = "Rainy Day. Emergency Savings."
+        rainyDay.birthYear = 2017
+        rainyDay.category = "Action ; Finance"
+        
+        let hearts = Emoji()
+        hearts.emojis = "💕"
+        hearts.emojiDefinition = "I Love you! Love It! Sending Love."
+        hearts.birthYear = 2018
+        hearts.category = "Love"
+        
+        return [crackingUp, blush, peace, kisses, cussing, why, impish, prayer, baby, bride, iDontKnow, never, dancing, haiti, money, credit, awesome, eyes, fist, duh, rainyDay, hearts]
+    }
 }
